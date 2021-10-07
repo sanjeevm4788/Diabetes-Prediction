491b018c685ded131fb0dbbc5516a09bed282bab
Sanjeev M <60375858+sanjeevm4788@users.noreply.github.com>
2021-10-07 18:49:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -33,5 +33,5 @@ def predict():
 
 if __name__ == '__main__':
 #Run the application
-    port = int(os.environ.get('PORT', 33507))
-    app.run(host='0.0.0.0', port=port, debug=True)
+  
+    app.run(debug = True)
